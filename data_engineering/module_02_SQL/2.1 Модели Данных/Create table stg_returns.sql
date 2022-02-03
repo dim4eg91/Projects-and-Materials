29f9995725918d8e30acdf547358a82baca12c5a
Дмитрий <kuzmin.dmitry2019@mail.ru>
2022-02-03 22:43:01 +0300

    re

--- data_engineering/module_02_SQL/2.1 Модели Данных/Create table stg_returns.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS stg.returns;
-CREATE TABLE stg.returns(
-   Person   VARCHAR(20) NOT NULL
-  ,Region   VARCHAR(20) NOT NULL
-);
-
-
-
-
--- data_engineering/module_02_SQL/2.1 Модели Данных/Insert into stg_people.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO stg.people(Person,Region) VALUES ('Anna Andreadi','West');
-INSERT INTO stg.people(Person,Region) VALUES ('Chuck Magee','East');
-INSERT INTO stg.people(Person,Region) VALUES ('Kelly Williams','Central');
-INSERT INTO stg.people(Person,Region) VALUES ('Cassandra Brandow','South');
