4fd71696c197cf6981ddd9705d5ef8290973fcc6
Дмитрий <kuzmin.dmitry2019@mail.ru>
2023-11-04 13:44:05 +0300

    add readme and edit main code

--- Python/connect_to_mail_server.py
+++ Python/connect_to_mail_server.py
@@ -35,6 +35,7 @@ def parsing_list_unseen_email(unseen_emails: str):
                 lst_id_unseen_emails.append(digit_char)
                 digit_char = ''
     lst_id_unseen_emails.append(digit_char)
+    lst_id_unseen_emails.pop()
 
     # for i in range(len(lst_id_unseen_emails)):
     #     res, msg = imap.fetch(f"b'{i}', '(RFC822)'")  # Для метода search по порядковому номеру письма
@@ -46,6 +47,27 @@ def parsing_list_unseen_email(unseen_emails: str):
     #
     # print('Заголовок письма:\n', decode_header(msg["Subject"])[0][0].decode(), '\n')  # чтение заголовка письма
 
+    return lst_id_unseen_emails
+
+
+def parsing_list_unseen_email_to_bytes(unseen_emails: str):
+    """
+    Функция принимает на вход строку непрочитанных писем и возвращает список ID таких писем
+    :param unseen_emails: объект, который содержит непрочитанные письма
+    :return: список ID непрочитанных писем
+    """
+
+    lst_id_unseen_emails = []
+    digit_char = ''
+    for i in range(len(unseen_emails)):
+        if unseen_emails[i].isdigit():
+            digit_char += f"b'{unseen_emails[i]}"
+        else:
+            if digit_char != '':
+                lst_id_unseen_emails.append(digit_char)
+                digit_char = ''
+    lst_id_unseen_emails.append(digit_char)
+    lst_id_unseen_emails.pop()
     return lst_id_unseen_emails
 
 
@@ -61,20 +83,33 @@ unseen_mails_str = str(unseen_mails[1])
 print('Непрочитанные письма: ',
       parsing_list_unseen_email(unseen_mails_str))  # формирование списка непрочитанных писем
 
-# res, msg = imap.fetch(b'3061', '(RFC822)')  # Для метода search по порядковому номеру письма
-# msg = email.message_from_bytes(msg[0][1])
-# print('Заголовок письма:\n', decode_header(msg["Subject"])[0][0].decode(), '\n')  # чтение заголовка письма
+#
+# print('Непрочитанные письма: ',
+#       parsing_list_unseen_email(unseen_mails_str))  # формирование списка непрочитанных писем
+
+# find_first_actual_unseen_email(parsing_list_unseen_email(unseen_mails_str))
+
+res, msg = imap.fetch(b'3061', '(RFC822)')  # Для метода search по порядковому номеру письма
+# print(type(b'3061'))
+msg = email.message_from_bytes(msg[0][1])
+print('Заголовок письма:\n', decode_header(msg["Subject"])[0][0].decode(), '\n')  # чтение заголовка письма
 
 # print(type(decode_header(msg["Subject"])[0][0].decode()))
-# s = ''.join(str(x) for x in body)
+
 # body = base64.b64decode(msg.get_payload()).decode('utf-8')  # тело (содержимое) письма
+# s = ''.join(str(x) for x in body)
 
 
-# print(msg.is_multipart())  # проверка письма на вложенность
+print(msg.is_multipart())  # проверка письма на вложенность
 #
 # for part in msg.walk():
 #     print(part.get_content_type())
 #
-# for part in msg.walk():
-#     if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'html':
-#         print(base64.b64decode(part.get_payload()).decode())
+for part in msg.walk():
+    if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'html':
+        print(base64.b64decode(part.get_payload()).decode())
+
+
+# пишу в файл тело письма, чтобы далее его распарсить и сделать DataFrame
+# with open('D:/pythonProject/simple.txt', 'w', encoding='utf-8') as f:
+#     f.write(body)
