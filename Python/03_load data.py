4573b5093e35b030bd821b82e23e6ccceb34b98b
Дмитрий <kuzmin.dmitry2019@mail.ru>
2023-11-21 02:45:43 +0300

    clear repo

---
+++ Python/03_load data.py
@@ -0,0 +1,24 @@
+import psycopg2
+import mail_config
+
+greenplum = psycopg2.connect(
+    database=mail_config.db_name,
+    user=mail_config.user,
+    password=mail_config.password,
+    host=mail_config.host,
+    port=mail_config.port
+)
+
+gp_cursor = greenplum.cursor()
+file = open('D:/Mail_read_files/email_body.csv', 'r', encoding='utf-8')
+
+
+def load_data_to_temp_table():
+    gp_cursor.copy_from(file, 'email_body_temp', sep=';')
+    greenplum.commit()
+
+
+load_data_to_temp_table()
+file.close()
+gp_cursor.close()
+greenplum.close()
